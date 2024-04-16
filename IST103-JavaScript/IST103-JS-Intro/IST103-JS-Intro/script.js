94dfa6fe6a0ad41a80b4116f5f7b45637f1747ce
GenerousSKA <35685716+GenerousSKA@users.noreply.github.com>
2024-04-16 16:26:24 -0700

    Add files via upload

---
+++ IST103-JavaScript/IST103-JS-Intro/IST103-JS-Intro/script.js
@@ -0,0 +1,17 @@
+//alert("Hello World");
+
+let personName = "Polina";
+let personAge = "18";
+const eyeColor = "blue";
+let isEmployed = true;
+
+console.log("A random person is: " + personName);
+
+let myBed = {
+  brand: "VITAFOAM",
+  color: "gray",
+  isUsed: true,
+  numberOfLegs: 6,
+};
+
+console.log("my table color is:" + myBed.color);
